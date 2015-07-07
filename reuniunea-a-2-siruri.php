9e099fe7d47c8dee6d7b2667358014bc3d090658
adiu85 <adiu85@users.noreply.github.com>
2015-07-07 19:36:01 +0300

    Update reuniunea-a-2-siruri.php

--- reuniunea-a-2-siruri.php
+++ reuniunea-a-2-siruri.php
@@ -28,7 +28,7 @@
 $arrayA = array("1", "8", "12", "7", "11", "5", "1", "12");
 $arrayB = array("13", "17", "49", "12", "1", "11");
 $arrayC = array();
-$arrayE=array();
+$arrayF=array();
 
 
 $countA = count($arrayA);
